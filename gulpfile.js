2816d99b2bfae8ca0373f7fe2b060aca1a56d2e2
Abdelhak <abdelhak@akermi.me>
2019-10-24 00:01:19 +0300

    to rm

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,111 @@
 // Load plugins
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del');
-const { watch, series } = require('gulp');
-// Styles
-gulp.task('styles', function() {
-    return sass('src/styles/main.scss', { style: 'expanded' })
-        .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('dist/styles'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(cssnano())
-        .pipe(gulp.dest('dist/styles'))
-        .pipe(notify({ message: 'Styles task complete' }));
-});
+const gulp = require("gulp"),
+  sass = require("gulp-sass");
+(autoprefixer = require("gulp-autoprefixer")),
+  (cssnano = require("gulp-cssnano")),
+  (jshint = require("gulp-jshint")),
+  (uglify = require("gulp-uglify")),
+  (imagemin = require("gulp-imagemin")),
+  (rename = require("gulp-rename")),
+  (concat = require("gulp-concat")),
+  (notify = require("gulp-notify")),
+  (cache = require("gulp-cache")),
+  (livereload = require("gulp-livereload")),
+  (del = require("del"));
 
-// Scripts
-gulp.task('scripts', function() {
-    return gulp.src('src/scripts/**/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/scripts'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts'))
-        .pipe(notify({ message: 'Scripts task complete' }));
-});
+const { watch, series, src, parallel } = require("gulp");
 
-// Images
-gulp.task('images', function() {
-    return gulp.src('src/images/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('dist/images'))
-        .pipe(notify({ message: 'Images task complete' }));
-});
+const files = {
+  stylesPath: "src/styles/**/*.scss",
+  scriptPath: "src/scripts/**/*.js",
+  imgPath: "src/images/**/*"
+};
 
-// Clean
-gulp.task('clean', function() {
-    return del(['dist/styles', 'dist/scripts', 'dist/images']);
-});
+/**
+ *
+ */
+function styles() {
+  return (
+    src(files.stylesPath)
+      .pipe(sass())
+      // .pipe(autoprefixer('last 2 version'))
+      .pipe(sass.sync().on("error", console.log))
+      .pipe(gulp.dest("dist/styles"))
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(cssnano())
+      .pipe(gulp.dest("dist/styles"))
+      .pipe(notify({ message: "Styles task complete" }))
+  );
+}
+
+/**
+ *
+ */
+function scripts() {
+  return src(files.scriptPath)
+    .pipe(jshint(".jshintrc"))
+    .pipe(jshint.reporter("default"))
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("dist/scripts"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/scripts"))
+    .pipe(notify({ message: "Scripts task complete" }));
+}
+
+/**
+ * images
+ * @todo add to watch func
+ */
+function images() {
+  return src(files.imgPath)
+    .pipe(
+      cache(
+        imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })
+      )
+    )
+    .pipe(gulp.dest("dist/images"))
+    .pipe(notify({ message: "Images task complete" }));
+}
+
+/**
+ * clean
+ */
+function clean() {
+  return del(["dist/styles", "dist/scripts", "dist/images"]);
+}
+
+function watchTask() {
+  watch(
+    [files.stylesPath, files.scriptPath, files.imgPath],
+    parallel(styles, scripts)
+  );
+}
+
+exports.default = series(clean, parallel(styles, scripts), watchTask);
 
 // Default task
-gulp.task('default', gulp.series('clean', function() {
-    gulp.start('styles', 'scripts', 'images');
-}));
+// gulp.task(
+//   "default",
+//   gulp.series("clean", function() {
+//     gulp.start("styles", "scripts", "images");
+//   })
+// );
 
 // Watch
-gulp.task('watch', function() {
+// function serve() {
+//   // Watch .scss files
+//   gulp.watch("src/styles/*.scss", series(styles));
 
-    // Watch .scss files
-    gulp.watch('src/styles/**/*.scss', series('styles'));
+//   // Watch .js files
+//   gulp.watch("src/scripts/**/*.js", series(scripts));
 
-    // Watch .js files
-    gulp.watch('src/scripts/**/*.js', series('scripts'));
+//   // Watch image files
+//   gulp.watch("src/images/**/*", series(images));
 
-    // Watch image files
-    gulp.watch('src/images/**/*', series('images'));
+//   // Create LiveReload server
+//   // livereload.listen();
 
-    // Create LiveReload server
-    livereload.listen();
-
-    // Watch any files in dist/, reload on change
-    gulp.watch(['dist/**']).on('change', livereload.changed);
-
-});
+//   // Watch any files in dist/, reload on change
+//   // gulp.watch(['dist/**']).on('change', livereload.changed);
+// }
